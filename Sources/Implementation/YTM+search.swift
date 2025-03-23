863ce021f807d40256d70f88729b9eab8575a484
EdGmr <e.gozembiler@mailbox.org>
2025-03-23 07:25:20 +0100

    started working on search implem

--- Sources/Implementation/YTM+search.swift
+++ Sources/Implementation/YTM+search.swift
@@ -4,9 +4,26 @@
 //
 //  Created by Eduard Gozembiler on 23.03.25.
 //
+import Foundation
 extension YouTubeMusic {
-    func searchSong(query: String) -> [String: String]{
-        return ["ss":""]
+    
+    func searchSong(query: String) async -> SearchResult<Song>?{
+        let body = Body(query: query)
+        
+        do{
+            let request = try YouTubeMusic.Request(body: body, method: "POST", url: DefaultParam.urlSearch, headers: DefaultParam.headers)
+            let requestPayload = try request.prepareRequest()
+            let data = try await request.fetch(from: requestPayload)
+            return nil
+        } catch YouTubeMusic.Error.invalidBody {
+            print("Invalid body in searchSong()")
+        } catch YouTubeMusic.Error.invalidURL {
+            print("Invalid URL in searchSong()")
+        } catch {
+            print("Error in searchSong(): \(error)")
+        }
+        return nil
+        
     }
 }
 
