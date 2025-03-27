ae40cb3b52156d6f3fdab5bb214a5bc6050bb46c
EdGmr <e.gozembiler@mailbox.org>
2025-03-28 00:02:49 +0100

    sdf

--- Sources/Classes/Parser.swift
+++ Sources/Classes/Parser.swift
@@ -12,41 +12,86 @@ final class Parser: Sendable{
     //TODO: id und so mit ner find funktion finden und die key:value paare dann dynamisch setzen
     //das hartgedotete muss raus
     
-    func extractInfo(obj: MusicShelfRenderer, option: SearchType) throws -> [def]{
-        var contents = obj.contents!
-
-        return obj.contents!.compactMap { item in
-            var views: String?
-            var duration: String?
-            let renderer = item.musicResponsiveListItemRenderer
-            let playlistItemData = item.musicResponsiveListItemRenderer?.playlistItemData
-            let videoID = item.musicResponsiveListItemRenderer?.playlistItemData?.videoID
-            let title = item.musicResponsiveListItemRenderer?.flexColumns?[0].musicResponsiveListItemFlexColumnRenderer?.text?.runs?[0].text!
-            let artist = item.musicResponsiveListItemRenderer?.flexColumns?[1].musicResponsiveListItemFlexColumnRenderer?.text?.runs?[0].text!
-            let name = item.musicResponsiveListItemRenderer?.flexColumns?[0].musicResponsiveListItemFlexColumnRenderer?.text?.runs?[0].text!
-            let browseId = item.musicResponsiveListItemRenderer?.navigationEndpoint?.browseEndpoint?.browseID
-            
-            if option == .video{
-                duration = (item.musicResponsiveListItemRenderer?.flexColumns?[1].musicResponsiveListItemFlexColumnRenderer?.text?.runs?[6].text)!
-                views = (item.musicResponsiveListItemRenderer?.flexColumns?[1].musicResponsiveListItemFlexColumnRenderer?.text?.runs?[4].text)!
-            } else {
-                duration = nil
-                views = nil
-            }
-            return def(
-                videoID: videoID,
-                title: title,
-                duration: duration,
-                browseId: browseId,
-                name: name,
-                views: views
-            )
-                
+    func extractMap<T: SearchResultType>(item: MusicShelfRendererContent, option: SearchType) -> T{
+        var views: String?
+        var duration: String?
+        let renderer = item.musicResponsiveListItemRenderer
+        let playlistItemData = item.musicResponsiveListItemRenderer?.playlistItemData
+        let videoID = item.musicResponsiveListItemRenderer?.playlistItemData?.videoID
+        let title = item.musicResponsiveListItemRenderer?.flexColumns?[0].musicResponsiveListItemFlexColumnRenderer?.text?.runs?[0].text!
+        let artist = item.musicResponsiveListItemRenderer?.flexColumns?[1].musicResponsiveListItemFlexColumnRenderer?.text?.runs?[0].text!
+        let name = item.musicResponsiveListItemRenderer?.flexColumns?[0].musicResponsiveListItemFlexColumnRenderer?.text?.runs?[0].text!
+        let browseId = item.musicResponsiveListItemRenderer?.navigationEndpoint?.browseEndpoint?.browseID
+        
+        if option == .video{
+            duration = (item.musicResponsiveListItemRenderer?.flexColumns?[1].musicResponsiveListItemFlexColumnRenderer?.text?.runs?[6].text)!
+            views = (item.musicResponsiveListItemRenderer?.flexColumns?[1].musicResponsiveListItemFlexColumnRenderer?.text?.runs?[4].text)!
+        } else {
+            duration = nil
+            views = nil
+        }
+        switch option{
+        case .song:
+            return Song(videoId: videoID, artist: artist, title: title, album: name, duration: duration, duration_seconds: nil, is_explicit: nil) as! T
+        case .video:
+            return Video(videoId: videoID!, title: title!, artist: artist!, views: views!, duration: duration!, duration_seconds: nil) as! T
+        default:
+            return Song(videoId: videoID, artist: artist, title: title, album: name, duration: duration, duration_seconds: nil, is_explicit: nil) as! T
         }
     }
     
+    func extractInfo<T: SearchResultType>(obj: MusicShelfRenderer, option: SearchType) -> [T]{
+        var contents = obj.contents!
+        return obj.contents!.compactMap { item in extractMap(item: item, option: option) as T }
+    }
+    
+    //func getSongs(contents: Contents, )
+    func extract<T: SearchResultType>(jsonData: Data, with option: SearchType) throws -> [T]{
+        let newRoot = try navigate(jsonData: jsonData, path: option.path)
+        let decoder = option.decodeType
+        let obj = try JSONDecoder().decode(decoder, from: navigate(jsonData: jsonData, path: option.path))
+        return try extract_default(obj: obj as! Root, option: option) as [T]
+    }
+    
+    func navigate(jsonData: Data, path: [String]) throws -> Data {
+        // First, parse the entire JSON
+        let json = try JSONSerialization.jsonObject(with: jsonData, options: [])
+        let result = try navigateRecursive(from: json, path: path)
+        return try JSONSerialization.data(withJSONObject: result)
+    }
+    
+    private func navigateRecursive(from json: Any, path: [String]) throws -> Any {
+        guard !path.isEmpty else {
+            return json
+        }
+        
+        let key = path[0]
+        let remainingPath = Array(path.dropFirst())
+        
+        switch json {
+        case let dict as [String: Any]:
+            guard let value = dict[key] else {
+                throw ParseError.pathNotFound(path: path)
+            }
+            return try navigateRecursive(from: value, path: remainingPath)
+            
+        case let array as [Any]:
+            // Handle numeric string index for arrays
+            if let index = Int(key), index >= 0 && index < array.count {
+                return try navigateRecursive(from: array[index], path: remainingPath)
+            }
+            throw ParseError.pathNotFound(path: path)
+            
+        default:
+            // If we've reached the end of the path, return the current value
+            guard remainingPath.isEmpty else {
+                throw ParseError.pathNotFound(path: path)
+            }
+            return json
+        }
+    }
     func extract_default<T: SearchResultType>(obj: Root, option: SearchType) throws -> [T]{
-        var store = [def]()
+        var store = [T]()
         var obj_ = obj.contents
         let tmp = obj_.filter { name in // filter only musicshelfitems
             if let name_ = name.itemSectionRenderer{
@@ -60,91 +105,13 @@ final class Parser: Sendable{
         try SearchType.allCases.forEach{ c in
             let objFiltered = try tmp.filter{ ($0.musicShelfRenderer?.title?.runs?[0].text?.contains(c.rawValue))!}
             if !objFiltered.isEmpty {
-                let tmpr = try extractInfo(obj: objFiltered[0].musicShelfRenderer!, option: c)
+                let tmpr = extractInfo(obj: objFiltered[0].musicShelfRenderer!, option: c) as [T]
                 store.append(contentsOf: tmpr)
             }
-            //extractInfo(obj: obj[0].musicShelfRenderer?, option: <#T##SearchType#>)
         }
-        
-        
-        //  let songs = tmp.filter{ item in category(name: "Songs", val: item)! }
-        //let artists = tmp.filter{ item in category(name: "Artists", val: item)! }
-        return store as! [T]
-    }
-    func category(name: String, val: Contents) -> Bool? {
-        return val.musicShelfRenderer?.title?.runs?[0].text?.contains(name)
-    }
-    //func getSongs(contents: Contents, )
-    
-    func extract<T: SearchResultType>(jsonData: Data, with option: SearchType) throws -> [T]{
-        let decoder = SearchType.song.decodeType
-        let path = SearchType.song.path
-        let obj_def = try parse(data: jsonData, path: SearchType.Default.path, decoder: SearchType.Default.decodeType) as! Root
-        //let obj = try parse(data: jsonData, path: path, decoder: decoder) as! MusicShelfRenderer
-        //return try extract_song(obj: obj, option: option)
-        return try extract_default(obj: obj_def, option: .Default) as [T]
-    }
-    
-    func parse<T: Codable>(data: Data, path: [String], decoder: T.Type) throws -> T{
-        let newRoot = try navigate(jsonData: data, path: path)
-        return try JSONDecoder().decode(decoder, from: newRoot)
-    }
-    
-    func navigate(jsonData: Data, path: [String]) throws -> Data {
-        // First, parse the entire JSON
-        let json = try JSONSerialization.jsonObject(with: jsonData, options: [])
-        let result = try navigateRecursive(from: json, path: path)
-        return try JSONSerialization.data(withJSONObject: result)
-    }
-    
-    private func navigateRecursive(from json: Any, path: [String]) throws -> Any {
-            guard !path.isEmpty else {
-                return json
-            }
-            
-            let key = path[0]
-            let remainingPath = Array(path.dropFirst())
-            
-            switch json {
-            case let dict as [String: Any]:
-                guard let value = dict[key] else {
-                    throw ParseError.pathNotFound(path: path)
-                }
-                return try navigateRecursive(from: value, path: remainingPath)
-            
-            case let array as [Any]:
-                // Handle numeric string index for arrays
-                if let index = Int(key), index >= 0 && index < array.count {
-                    return try navigateRecursive(from: array[index], path: remainingPath)
-                }
-                throw ParseError.pathNotFound(path: path)
-            
-            default:
-                // If we've reached the end of the path, return the current value
-                guard remainingPath.isEmpty else {
-                    throw ParseError.pathNotFound(path: path)
-                }
-                return json
-            }
-        }
-}
-
-protocol Extract{
-    //associatedtype ResultItem: SearchResultType
-   // var contents: [ParseObjects] { get }
-    func extract<T: SearchResultType>(option: SearchType) throws -> [T]
-}
-extension Extract{
-    func extract<T: SearchResultType>(obj: Codable, option: SearchType) throws -> [T]{
-        switch option{
-        case .song:
-            return [def()] as! [T]
-        default:
-            return [def()] as! [T]
-        }
+        return store
     }
 }
-
 
 extension SearchType{
     var path: [String] {
